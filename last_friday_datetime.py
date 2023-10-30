e267d1a60e21f88b293f894ff23494874459a70d
Esoner13 <44065425+Esoner13@users.noreply.github.com>
2023-10-30 18:58:46 +0300

    Update last_friday_datetime.py

--- last_friday_datetime.py
+++ last_friday_datetime.py
@@ -1,7 +1,7 @@
 import datetime
 from dateutil.relativedelta import FR, relativedelta
 
-x= input()
+x= "08/2022"
 def get_last_friday(d):
     d = datetime.datetime.strptime("01/" + d, "%d/%m/%Y")
     res = d + relativedelta(day=31, weekday=FR(-1))
